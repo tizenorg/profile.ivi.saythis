71f490d1a931318a06d97c89eea3ac2c415e1a37
brianjjones <brian.j.jones@intel.com>
2013-09-12 13:03:36 -0700

    Updating saythis to use the tizen.speech.vocalizeString() method instead of websockets
    Change-Id: I1a93fa5f81ac73c280a131fdd7fadc34a982930e
    Signed-off-by: brianjjones <brian.j.jones@intel.com>

--- js/main.js
+++ js/main.js
@@ -1,54 +1,30 @@
 /*
  * Copyright (c) 2012, Intel Corporation.
  *
- * This program is licensed under the terms and conditions of the 
+ * This program is licensed under the terms and conditions of the
  * Apache License, version 2.0.  The full text of the Apache License is at
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  */
+var mySpeechObj = tizen.speech;
 
-App = {};
+$(document).ready(function()
+{
+   $('#speakButton').click(function() {
 
-(function () {
-    function init() {
-        /* initialization */
-        //license_init('license', "main_page");
-    }
+            var text = $('#inputArea').val();
+            console.log(text);
 
-    function onSpeakResults(results) {
-        console.log(results);
-        $('#speakButton').removeClass('pressed');
-    }
-
-    function checkAPI() {
-        var result = wsAPI.isReady();
-        if (!result) {
-            console.log('Cannot connect to websocket API daemon');
-            wsAPI.connect();
-        }
-        return result;
-    }
-
-    $(document).ready(function()
-    {
-		init();
-
-        $('#speakButton').click(function() {
-            if (checkAPI()) {
-                var text = $('#inputArea').val();
-                console.log(text);
-                if (text && !$('#speakButton').hasClass('pressed')) {
-                    wsAPI.speak(text, onSpeakResults);
-                    $('#speakButton').addClass('pressed');
-                }
+            if (text) {
+               mySpeechObj.vocalizeString(text);
             }
-            console.log('Speak button clicked');
-        });
 
-	$('#inputArea').keyup(function(event) {
-		if (event.keyCode === 13) {
-			$('#speakButton').click();
-		}
-	});
+	    $('#inputArea').val("");
     });
-})()
+
+    $('#inputArea').keyup(function(event) {
+            if (event.keyCode === 13) {
+                    $('#speakButton').click();
+             }
+    });
+});
--- packaging/saythis.spec
+++ packaging/saythis.spec
@@ -6,9 +6,9 @@ Group:      Applications/System
 License:    Apache 2.0
 URL:        http://www.tizen.org2
 Source0:    %{name}-%{version}.tar.bz2
-Requires:   remotecontrol
-Requires:   webskeleton
+BuildRequires:  zip
 BuildRequires:  desktop-file-utils
+Requires:   wrt-installer
 
 %description
 A HTML5 speech to text app
@@ -18,18 +18,19 @@ A HTML5 speech to text app
 
 %build
 
-make %{?jobs:-j%jobs}
+make wgtPkg
+
+#make %{?jobs:-j%jobs}
 
 %install
 rm -rf %{buildroot}
 %make_install
 
-desktop-file-install --delete-original       \
-  --dir %{buildroot}%{_datadir}/applications             \
-   %{buildroot}%{_datadir}/applications/*.desktop
+%post
+if [ -f /opt/usr/apps/.preinstallWidgets/preinstallDone ]; then
+    wrt-installer -i /opt/usr/apps/.preinstallWidgets/saythis.wgt;
+fi
 
 %files
 %defattr(-,root,root,-)
-%{_datadir}/saythis
-%{_datadir}/applications/saythis.desktop
-%{_datadir}/pixmaps/saythis.png
+/opt/usr/apps/.preinstallWidgets/saythis.wgt
